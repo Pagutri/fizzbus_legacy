5ce6ea1e7b2bc8924b36320808b02bbd0f6703bb
Pa Gutri <pagutri@gmail.com>
2022-05-04 16:44:07 -0500

    Add test for score divisible by 5

--- test/lib/services/FizzBuzzService.test.js
+++ test/lib/services/FizzBuzzService.test.js
@@ -7,9 +7,16 @@ describe("Tests for FizzBuzzService", () => {
         const explorer1AfterFizzBuzz = FizzBuzzService.applyValidationInExplorer(explorer1)
         expect(explorer1AfterFizzBuzz.trick).toBe(1)
     })
+
     test("2. Test trick attribute for explorer with score divisible only by 3", () => {
         const explorer3 = {name: "Explorer3", score: 3}
         const explorer3AfterFizzBuzz = FizzBuzzService.applyValidationInExplorer(explorer3) 
         expect(explorer3AfterFizzBuzz.trick).toBe("FIZZ")
+    })
+
+    test("3. Test trick attribute for explorer with score divisible only by 5", () => {
+        const explorer5 = {name: "Explorer5", score: 5}
+        const explorer5AfterFizzBuzz = FizzBuzzService.applyValidationInExplorer(explorer5) 
+        expect(explorer5AfterFizzBuzz.trick).toBe("BUZZ")
     })
 })
