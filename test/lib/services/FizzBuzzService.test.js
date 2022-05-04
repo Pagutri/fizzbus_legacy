eef28128d5a71b24a4fc152a61eb1d5b21adb812
Pa Gutri <pagutri@gmail.com>
2022-05-04 16:39:17 -0500

    Correct typo

--- test/lib/services/FizzBuzzService.test.js
+++ test/lib/services/FizzBuzzService.test.js
@@ -4,7 +4,7 @@ const FizzBuzzService = require('../../../app/lib/services/FizzBuzzService.js');
 describe("Tests for FizzBuzzService", () => {
     test("1. Test trick attribute for explorer with score non-divisible by 3 nor 5", () => {
         const explorer1 = {name: "Explorer1", score: 1}
-        const explorer1AfterFizzBuzz = FizzbuzzService.applyValidationInExplorer(explorer1)
+        const explorer1AfterFizzBuzz = FizzBuzzService.applyValidationInExplorer(explorer1)
         expect(explorer1AfterFizzBuzz.trick).toBe(1)
     })
 })
