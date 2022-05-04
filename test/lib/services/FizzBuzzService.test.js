e57662cf159069e9b146cf2fd5f51c6468085a6d
Pa Gutri <pagutri@gmail.com>
2022-05-04 16:23:55 -0500

    Simplified first test

--- test/lib/services/FizzBuzzService.test.js
+++ test/lib/services/FizzBuzzService.test.js
@@ -2,21 +2,9 @@ const Reader = require('../../../app/lib/utils/Reader.js');
 const FizzBuzzService = require('../../../app/lib/services/FizzBuzzService.js');
 
 describe("Tests for FizzBuzzService", () => {
-    test("1. Test fizzbuzz validation for a given explorer", () => {
+    test("1. Test trick attribute for explorer with score non-divisible by 3 nor 5", () => {
         const explorer1 = {name: "Explorer1", score: 1}
         FizzbuzzService.applyValidationInExplorer(explorer1)
         expect(explorer1.trick).toBe(1)
-
-        const explorer3 = {name: "Explorer3", score: 3}
-        FizzbuzzService.applyValidationInExplorer(explorer3)
-        expect(explorer3.trick).toBe("FIZZ")
-
-        const explorer5 = {name: "Explorer5", score: 5}
-        FizzbuzzService.applyValidationInExplorer(explorer5)
-        expect(explorer5.trick).toBe("BUZZ")
-
-        const explorer15 = {name: "Explorer15", score: 15}
-        FizzbuzzService.applyValidationInExplorer(explorer15)
-        expect(explorer15.trick).toBe("FIZZBUZZ")
     })
 })
