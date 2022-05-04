3af3bfb3b220ddb83a0812fdd1060eb184c06fec
Pa Gutri <pagutri@gmail.com>
2022-05-04 13:56:24 -0500

    Read testfile in test

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -2,7 +2,22 @@ const Reader = require('../../../app/lib/utils/Reader.js');
 
 describe("Tests for Reader", () => {
     test("1. Read the file correctly", () => {
-        const explorers = Reader.readJsonFile("explorers.json");
-        expect(explorers[0].name).toBe("Woopa1")
+        const explorers = Reader.readJsonFile("test/lib/utils/Reader_testfile.json");
+        expect(explorers).toStrictEqual([
+            {
+              "name": "Woopa1",
+              "stacks": [
+                "javascript",
+                "reasonML"
+              ]
+            },
+            {
+              "name": "Woopa2",
+              "stacks": [
+                "javascript",
+                "groovy"
+              ]
+            }
+        ])
     });
 });
