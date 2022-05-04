716bd8a19a70042f4a5e4c2d67fd34d91ca26e82
Pa Gutri <pagutri@gmail.com>
2022-05-04 13:26:24 -0500

    Change path because jest reads from root

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -2,7 +2,7 @@ const Reader = require('../../../app/lib/utils/Reader.js');
 
 describe("Tests for Reader", () => {
     test("1. Read the file correctly", () => {
-        const explorers = Reader.readJsonFile("../../../explorers.json");
+        const explorers = Reader.readJsonFile("explorers.json");
         expect(explorers[0].name).toBe("Woopa1")
     });
 });
