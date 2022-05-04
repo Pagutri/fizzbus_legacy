5ba19c32138a7ddb174fdf6db2d16e5d6c94fea3
Pa Gutri <pagutri@gmail.com>
2022-05-04 16:28:40 -0500

    Save result of aplying service to a variable

--- test/lib/services/FizzBuzzService.test.js
+++ test/lib/services/FizzBuzzService.test.js
@@ -4,7 +4,7 @@ const FizzBuzzService = require('../../../app/lib/services/FizzBuzzService.js');
 describe("Tests for FizzBuzzService", () => {
     test("1. Test trick attribute for explorer with score non-divisible by 3 nor 5", () => {
         const explorer1 = {name: "Explorer1", score: 1}
-        FizzbuzzService.applyValidationInExplorer(explorer1)
-        expect(explorer1.trick).toBe(1)
+        const explorer1AfterFizzBuzz = FizzbuzzService.applyValidationInExplorer(explorer1)
+        expect(explorer1AfterFizzBuzz.trick).toBe(1)
     })
 })
