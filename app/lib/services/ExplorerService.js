ea5b72c9412fc91430260b13d0eebb4cef45c7fd
Pa Gutri <pagutri@gmail.com>
2022-05-04 14:19:51 -0500

    Add getAmountOfExplorersByMission() method

--- app/lib/services/ExplorerService.js
+++ app/lib/services/ExplorerService.js
@@ -2,6 +2,10 @@ class ExplorerService {
     static filterByMission(explorers, mission) {
         return explorers.filter((explorer) => explorer.mission == mission);
     }
+
+    static getAmountOfExplorersByMission(explorers, mission) {
+        return explorers.filter((explorer) => explorer.mission == mission).length;
+    }
 }
 
 module.exports = ExplorerService
