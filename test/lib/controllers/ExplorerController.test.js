9ff7793d6b8c6346d73558e7f04d4415ac358712
Pa Gutri <pagutri@gmail.com>
2022-05-05 09:58:38 -0500

    Add test for getExplorersUsernamesByMission

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -5,4 +5,9 @@ describe("Tests for ExplorerController", () => {
         const explorersInNode = ExplorerController.getExplorersByMission("node");
         expect(explorersInNode[5].name).toBe("Woopa11");
     });
+
+    test("2. Test for getExplorersUsernamesByMission", () => {
+        const usernames = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(usernames[6]).toBe("ajolonauta12");
+    });
 });
