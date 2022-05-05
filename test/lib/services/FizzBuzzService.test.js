925ece14533a4b9bda8c0744fc4aa9f54e651f39
Pa Gutri <pagutri@gmail.com>
2022-05-05 13:06:56 -0500

    Add test for applyValidationInNumber when it receives a number divisible by 3 and 5

--- test/lib/services/FizzBuzzService.test.js
+++ test/lib/services/FizzBuzzService.test.js
@@ -39,4 +39,9 @@ describe("Tests for FizzBuzzService", () => {
         const trickForScore5 = FizzBuzzService.applyValidationInNumber(5);
         expect(trickForScore5).toBe("BUZZ");
     });
+
+    test("8. Test trick returned for a score instead of an explorer, when score is divisible only by 3 and 5", () => {
+        const trickForScore15 = FizzBuzzService.applyValidationInNumber(15);
+        expect(trickForScore15).toBe("FIZZBUZZ");
+    });
 });
